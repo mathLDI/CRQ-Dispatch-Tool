f8f5a4864e1f10567812d21e147468aa1f385375
Mathieu Laurin <laurinmathieu@hotmail.com>
2023-11-08 17:22:03 -0500

    Your commit message

--- src/Component/Pages/signIn.jsx
+++ src/Component/Pages/signIn.jsx
@@ -24,6 +24,14 @@ export default function SignIn({ auth }) {
     }
   }
 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the form from being submitted via traditional HTTP request
+    handleSignIn();
+  }
+
+
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -39,7 +47,7 @@ export default function SignIn({ auth }) {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form className="space-y-6" action="" method="POST" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -53,8 +61,8 @@ export default function SignIn({ auth }) {
                   required
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                  className="block w-full rounded-md border-0 py-2.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
               </div>
             </div>
 
@@ -63,11 +71,9 @@ export default function SignIn({ auth }) {
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    {/**Forgot password? */}
-                  </a>
-                </div>
+
+            
+               
               </div>
               <div className="mt-2">
                 <input
@@ -78,21 +84,21 @@ export default function SignIn({ auth }) {
                   required
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                  className="block w-full rounded-md border-0 py-2.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
               </div>
             </div>
 
             <div>
               <button
-                type="button" // Change this to "button" to prevent form submission
-                onClick={handleSignIn} // Handle sign-in when the button is clicked
+                type="submit" // Change this to "submit" to trigger form submission
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
-            
+
+
             {error && ( // Display the error message when available
               <div className="mt-4 text-red-600 text-sm">{error}</div>
             )}
